32dbd9998925f8617e8db224944cffef49c3936d
Volker Krebs <vk@abas.de>
2011-09-13 15:46:36 +0200

    spawn msggen

--- mochitests/first/src/msggen.erl
+++ mochitests/first/src/msggen.erl
@@ -1,5 +1,5 @@
 -module(msggen).
--export([start/3]).
+-export([start/3,massiveAttack/0,massiveAttack/1]).
 
 start(0, _, _) -> ok;
 start(Num, Interval, Max) ->
@@ -7,3 +7,13 @@ start(Num, Interval, Max) ->
 	Msg = io_lib:format("Fake message #~w", [Num]),
 	router:send(Id, Msg),
 	receive after Interval -> start(Num -1, Interval, Max) end.
+
+massiveAttack() -> massiveAttack(100).
+
+massiveAttack(Num) ->
+	[ 
+	spawn(fun() -> 
+		msggen:start(1000000, 100, 10000)
+	end)
+	|| _ <- lists:seq(1,Num) 
+	].
