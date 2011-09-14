16781d5f7ee4d053221a2e25e5f385edd86177fe
Volker Krebs <volker.krebs@abas.de>
2011-09-14 16:42:22 +0200

    Days in own function

--- DigiDoodle/tut11.erl
+++ DigiDoodle/tut11.erl
@@ -13,22 +13,8 @@ month_length(Year, Month) ->
             leap;
         true ->
             not_leap
-    end,  
-    case Month of
-        sep -> 30;
-        apr -> 30;
-        jun -> 30;
-        nov -> 30;
-        feb when Leap == leap -> 29;
-        feb -> 28;
-        jan -> 31;
-        mar -> 31;
-        may -> 31;
-        jul -> 31;
-        aug -> 31;
-        oct -> 31;
-        dec -> 31
-    end.
+    end,
+	month(Month,Leap).
 %%
 %%
 %%With rem operator
@@ -44,18 +30,23 @@ month_length2(Year,Month) ->
 		true ->
 			not_leap
 	end,
-    case Month of
-        sep -> 30;
-        apr -> 30;
-        jun -> 30;
-        nov -> 30;
-        feb when Leap == leap -> 29;
-        feb -> 28;
-        jan -> 31;
-        mar -> 31;
-        may -> 31;
-        jul -> 31;
-        aug -> 31;
-        oct -> 31;
-        dec -> 31
-    end.
+	month(Month,Leap).
+%%
+%%
+month(Month,Leap) ->
+	case Month of
+		sep -> 30;
+		apr -> 30;
+		jun -> 30;
+		nov -> 30;
+		feb when Leap == leap -> 29;
+		feb -> 28;
+		jan -> 31;
+		mar -> 31;
+		may -> 31;
+		jul -> 31;
+		aug -> 31;
+		oct -> 31;
+		dec -> 31
+	end.
+
