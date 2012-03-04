5f588cefd9f490e22e1efdd46171d7775cb2e094
Stefan Gebhardt <stefan.gebhardt@finux.net>
2012-03-04 01:22:26 +0100

    started ui

--- ccdexpress/public/js/script.js
+++ ccdexpress/public/js/script.js
@@ -1,8 +1,50 @@
-/* Author:
+/* Author:Stefan */
 
-*/
-
-
-
-
-
+App = function() {
+};
+App.prototype.listAll = function() {
+	var errorBack = function(xhr, text, error) {
+		console.log('Error while listing');
+	};
+	var render = function(users) {
+		// wipe out here ?
+		$('.plate').remove();
+		$.each(users, function(index, user) {
+			var colorType = user.phase === 'black' ? 'dark' : 'light';
+			var plate = 
+				$('<div>').addClass('plate')
+					.addClass(colorType)
+					.css('background', user.phase)
+					.append($('<div>').addClass('name').text(user.name))
+					.append($('<div>').addClass('day').text(user.day));
+			$('[role="main"]').append(plate);
+		});
+	};
+	$.ajax({
+		url: '/users',
+		type: 'GET',
+		dataType: 'json',
+		error: errorBack,
+		success: render
+	});
+};
+App.prototype.join = function(name) {
+	if ( !name || typeof name != 'string' || name.trim() == '' ) {
+		alert('Who are You?');
+		return;
+	}
+	var self = this;
+	var user = {
+		'name': name,
+		'phase': 'black',
+		'day': 0
+	};
+	$.ajax({
+		url: '/users',
+		type: 'PUT',
+		dataType: 'json',
+		data: user,
+		error: function() { alert('Oops!'); },
+		success: self.listAll
+	});
+}; 
