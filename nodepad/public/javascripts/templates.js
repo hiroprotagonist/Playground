39c99804accf2f3b7d474528f669718f557b0310
Stefan Gebhardt <stefan.gebhardt@finux.net>
2012-03-31 01:03:26 +0200

    still working

---
+++ nodepad/public/javascripts/templates.js
@@ -0,0 +1,9 @@
+var template = {
+	you: '<div class="plate" date-phase="<%= data.phase %>" id="<%= data._id %>">\
+			<div class="name"><%= data.name %></div>\
+			<div class="day"><%= data.day %></div>\
+			<button data-role="increase-day">+</button>\
+		</div>'
+}
+
+
