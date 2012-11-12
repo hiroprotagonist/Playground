542585d364e01d333c5de5b92b80e0bcdf9ceadc
Stefan Gebhardt <stefan.gebhardt@finux.net>
2012-11-12 17:38:46 +0100

    Update d.js

--- d.js
+++ d.js
@@ -5,23 +5,12 @@ $(document).bind( 'mobileinit', function () {
 
 var dueker = {
 	init_lvchumlvers : function () {
-		$('#dueker_lvchumlvers_page').live("pageshow", function() {
-			if ($('#dueker_lvchumlvers_nve').attr('value') == '') {
-				$('#dueker_lvchumlvers_nve').focus();
-			} else {
-				$('#dueker_lvchumlvers_zuplatz').focus();
-			}
-			$('.dueker_lvchumlvers_page_errorMsgDialog').popup("open");
-		});
-//		$('#dueker_lvchumlvers_page').live("updatelayout", function() {
-//			var errMsg = $('#dueker_lvchumlvers_page_errorMsgDialog').children().length;
-//			if (errMsg > 0) {
-//				$('#dueker_lvchumlvers_page_errorMsgDialog').popup("open");
-//			}
-//		});
 
-		$('.dueker_lvchumlvers_page').live("pageload", function() {
+		$('.dueker_lvchumlvers_page').live( "pageshow", function() {
 			var dlg = $('.dueker_lvchumlvers_page_errorMsgDialog');
+			
+			console.log('ps dlg=' + dlg);
+			
 			if ( typeof dlg !== 'undefined' ) {
 				$( dlg ).popup("open");
 			}
